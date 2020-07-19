1b2f87a981abc6fda383891268148ab9c15fc497
dannymotherofdragons <ma.ines.santos@gmail.com>
2020-07-19 22:22:34 +0100

    spawned trees and oranges

--- main.js
+++ main.js
@@ -1,74 +1,368 @@
-var scene, camera, mesh, renderer;
-var meshFloor;
-var keyboard = {};
-var player = {
-    height: 1.8,
-    speed: 0.2,
-}
+var scene = null,
+    camera,
+    renderer = null,
+    floor,
+    keyboard = {},
+    player = {
+        height: 1.8,
+        speed: 0.2,
+    }
 
-function init() {
-    //set up da cena, camera e mesh
+window.onload = function init() {
+    //set up da cena, camara e mesh
+
+    renderer = new THREE.WebGLRenderer;
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor("#34c3eb");
+    document.body.appendChild(renderer.domElement);
+
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(90, 1280 / 720, 0.1, 1000);
 
-    mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshPhongMaterial({
-            color: 0xff9999,
-            wireframe: false
-        })
-    );
-    scene.add(mesh);
+    window.addEventListener('keydown', keyDown);
+    window.addEventListener('keyup', keyUp);
 
-    createFloor()
-    
+    spawnArvores();
+    //spawnFire();
+    spawnFruit();
+    createFloor();
+
     //luzes
-    ambientLight = new THREE.AmbientLight(0xfffff,0.7   );
+    ambientLight = new THREE.AmbientLight(0xfffff, 1);
     scene.add(ambientLight);
 
     //camera position
-    camera.position.set(0, player.height, -5);
+    camera.position.set(0, player.height, 0);
     camera.lookAt(new THREE.Vector3(0, player.height, 0));
 
-    //render function
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
+    //sombras
     renderer.shadowMap.type = THREE.BasicShadowMap;
     document.body.appendChild(renderer.domElement);
 
     animate();
 }
 
-function createFloor(){
-    meshFloor = new THREE.Mesh(
-        new THREE.PlaneGeometry(45, 37, 10),
-        new THREE.MeshPhongMaterial({
-            color: 0x42a832,
-            wireframe: false
-        })
-    )
-    meshFloor.rotation.x -= Math.PI / 2; //roda -90 graus em x
-    scene.add(meshFloor)
-    }
+function createFloor() {
+
+    let grassGeometry = new THREE.PlaneGeometry(70, 70, 20);
+    let grassMesh = new THREE.MeshPhongMaterial({
+        color: 0x42a832
+    })
+    let floor = new THREE.Mesh(grassGeometry, grassMesh);
+
+    floor.rotation.x -= Math.PI / 2; //roda -90 graus em x*/
+    scene.add(floor)
+}
+
+function spawnArvores() {
+    // Tronco
+    let troncoGeo = new THREE.CylinderGeometry(0.5, 0.5, 8)
+    let troncoTex = new THREE.TextureLoader().load("./img/tronco.jpg")
+    let troncoMaterial = new THREE.MeshPhongMaterial({
+        map: troncoTex
+    })
+
+    // Folhas
+    let copaGeo = new THREE.SphereGeometry(4, 4, 20)
+    let copaTex = new THREE.TextureLoader().load("./img/tree.jpg")
+    let copaMaterial = new THREE.MeshPhongMaterial({
+        map: copaTex
+    })
+
+    arvore = new THREE.Object3D()
+    //Tronco
+    let tronco = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco.position.y = 3
+    arvore.add(tronco)
+    //Folhas
+    let copa = new THREE.Mesh(copaGeo, copaMaterial)
+    copa.position.y = 3
+    tronco.add(copa)
+    arvore.position.set(4, 1, 15)
+    scene.add(arvore)
+
+    arvore1 = new THREE.Object3D()
+    //Tronco
+    let tronco1 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco1.position.y = 3
+    arvore1.add(tronco1)
+    //Folhas
+    let copa1 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa1.position.y = 3
+    tronco1.add(copa1)
+    arvore1.position.set(20, 1, 15)
+    scene.add(arvore1)
+
+    arvore2 = new THREE.Object3D()
+    //Tronco
+    let tronco2 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco2.position.y = 3
+    arvore2.add(tronco2)
+    //Folhas
+    let copa2 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa2.position.y = 3
+    tronco2.add(copa2)
+    arvore2.position.set(-20, 1, -15)
+    scene.add(arvore2)
+
+    arvore3 = new THREE.Object3D()
+    //Tronco
+    let tronco3 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco3.position.y = 3
+    arvore3.add(tronco3)
+    //Folhas
+    let copa3 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa3.position.y = 3
+    tronco3.add(copa3)
+    arvore3.position.set(-14, 1, 15)
+    scene.add(arvore3)
+
+    arvore4 = new THREE.Object3D()
+    //Tronco
+    let tronco4 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco4.position.y = 3
+    arvore4.add(tronco4)
+    //Folhas
+    let copa4 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa4.position.y = 3
+    tronco4.add(copa4)
+    arvore4.position.set(4, 1, 0)
+    scene.add(arvore4)
+
+    arvore5 = new THREE.Object3D()
+    //Tronco
+    let tronco5 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco5.position.y = 3
+    arvore5.add(tronco5)
+    //Folhas
+    let copa5 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa5.position.y = 3
+    tronco5.add(copa5)
+    arvore5.position.set(30, 1, -10)
+    scene.add(arvore5)
+
+    arvore6 = new THREE.Object3D()
+    //Tronco
+    let tronco6 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco6.position.y = 3
+    arvore6.add(tronco6)
+    //Folhas
+    let copa6 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa6.position.y = 3
+    tronco6.add(copa6)
+    arvore6.position.set(25, 1, 5)
+    scene.add(arvore6)
+
+    arvore7 = new THREE.Object3D()
+    //Tronco
+    let tronco7 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco7.position.y = 3
+    arvore7.add(tronco7)
+    //Folhas
+    let copa7 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa7.position.y = 3
+    tronco7.add(copa7)
+    arvore7.position.set(20, 1, 34)
+    scene.add(arvore7)
+
+    arvore8 = new THREE.Object3D()
+    //Tronco
+    let tronco8 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco8.position.y = 3
+    arvore8.add(tronco8)
+    //Folhas
+    let copa8 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa8.position.y = 3
+    tronco8.add(copa8)
+    arvore8.position.set(-30, 1, -20)
+    scene.add(arvore8)
+
+    arvore9 = new THREE.Object3D()
+    //Tronco
+    let tronco9 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco9.position.y = 3
+    arvore9.add(tronco9)
+    //Folhas
+    let copa9 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa9.position.y = 3
+    tronco9.add(copa9)
+    arvore9.position.set(10, 1, -20)
+    scene.add(arvore9)
+
+    arvore10 = new THREE.Object3D()
+    //Tronco
+    let tronco10 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco10.position.y = 3
+    arvore10.add(tronco10)
+    //Folhas
+    let copa10 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa10.position.y = 3
+    tronco10.add(copa10)
+    arvore10.position.set(24, 1, -15)
+    scene.add(arvore10)
+
+    arvore11 = new THREE.Object3D()
+    //Tronco
+    let tronco11 = new THREE.Mesh(troncoGeo, troncoMaterial)
+    tronco11.position.y = 3
+    arvore11.add(tronco11)
+    //Folhas
+    let copa11 = new THREE.Mesh(copaGeo, copaMaterial)
+    copa11.position.y = 3
+    tronco11.add(copa11)
+    arvore11.position.set(0, 1, 30)
+    scene.add(arvore11)
+
+}
+
+//ideia inicial era apagar fogueiras na floresta
+
+/*function spawnFire() {
+    let fireGeo = new THREE.SphereGeometry(1,1, 20)
+    let fireMaterial = new THREE.MeshLambertMaterial({
+        color: 0xfc6603
+    })
+
+    let fireLogGeo = new THREE.CylinderGeometry(0.1, 0.1, 4)
+    let fireLogTex = new THREE.TextureLoader().load("./img/tronco.jpg")
+    let fireLogMaterial = new THREE.MeshPhongMaterial({
+        map: fireLogTex
+    })
+
+    fire = new THREE.Object3D()
+
+    let fireLog = new THREE.Mesh(fireLogGeo, fireLogMaterial)
+    fireLog.rotation.x -= Math.PI / 2;
+    fireLog.position.y = 0.5
+    fire.add(fireLog);
+
+    let fireFlame = new THREE.Mesh(fireGeo, fireMaterial)
+    fireFlame.position.y = 1
+    fire.add(fireFlame);
+
+    scene.add(fire)
+
+}*/
+
+function spawnFruit() {
+    let fruitGeo = new THREE.SphereGeometry(0.5, 0.5, 40);
+    let fruitTex = new THREE.TextureLoader().load("./img/orange.jpg")
+    let fruitMaterial = new THREE.MeshPhongMaterial({
+        map: fruitTex
+    })
+
+    fruitSpawned = new THREE.Object3D
+
+    let fruit = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit.position.set(5, 0.5, 15.5)
+    fruitSpawned.add(fruit)
+    scene.add(fruitSpawned)
+
+    fruitSpawned1 = new THREE.Object3D
+
+    let fruit1 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit1.position.set(21, 0.5, 15.5)
+    fruitSpawned.add(fruit1)
+    scene.add(fruitSpawned)
+
+    fruitSpawned2 = new THREE.Object3D
+
+    let fruit2 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit2.position.set(-19, 0.5, -14.5)
+    fruitSpawned.add(fruit2)
+    scene.add(fruitSpawned)
+
+    fruitSpawned3 = new THREE.Object3D
+
+    let fruit3 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit3.position.set(-13, 0.5, 14.5)
+    fruitSpawned.add(fruit3)
+    scene.add(fruitSpawned)
+
+    fruitSpawned4 = new THREE.Object3D
+
+    let fruit4 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit4.position.set(5, 0.5, 0.5)
+    fruitSpawned.add(fruit4)
+    scene.add(fruitSpawned)
+
+    fruitSpawned5 = new THREE.Object3D
+
+    let fruit5 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit5.position.set(31, 0.5, -9.5)
+    fruitSpawned.add(fruit5)
+    scene.add(fruitSpawned)
+
+    fruitSpawned6 = new THREE.Object3D
+
+    let fruit6 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit6.position.set(26, 0.6, 5.5)
+    fruitSpawned.add(fruit6)
+    scene.add(fruitSpawned)
+
+    fruitSpawned6 = new THREE.Object3D
+
+    let fruit7 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit7.position.set(21, 0.5, 34.5)
+    fruitSpawned.add(fruit7)
+    scene.add(fruitSpawned)
+
+    fruitSpawned8 = new THREE.Object3D
+
+    let fruit8 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit8.position.set(-29,0.5, -19)
+    fruitSpawned.add(fruit8)
+    scene.add(fruitSpawned)
+
+    fruitSpawned9 = new THREE.Object3D
+
+    let fruit9 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit9.position.set(26, 0.6, 5.5)
+    fruitSpawned.add(fruit9)
+    scene.add(fruitSpawned)
+
+    fruitSpawned10 = new THREE.Object3D
+
+    let fruit10 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit10.position.set(25, 0.5, -14.5)
+    fruitSpawned.add(fruit10)
+    scene.add(fruitSpawned)
+
+    fruitSpawned11 = new THREE.Object3D
+
+    let fruit11 = new THREE.Mesh(fruitGeo, fruitMaterial)
+
+    fruit11.position.set(1, 0.5, 31)
+    fruitSpawned.add(fruit11)
+    scene.add(fruitSpawned)
+}
 
 
 //animate
 function animate() {
     requestAnimationFrame(animate);
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-
     //keyboard event functions
 
     if (keyboard[87]) { //s key
-        camera.position.x += Math.sin(camera.rotation.y) * player.speed
-        camera.position.z += Math.cos(camera.rotation.y) * player.speed
-    }
-    if (keyboard[83]) { //w key
         camera.position.x -= Math.sin(camera.rotation.y) * player.speed
         camera.position.z -= Math.cos(camera.rotation.y) * player.speed
+    }
+    if (keyboard[83]) { //w key
+        camera.position.x += Math.sin(camera.rotation.y) * player.speed
+        camera.position.z += Math.cos(camera.rotation.y) * player.speed
     }
 
     if (keyboard[65]) { //left movement
@@ -90,8 +384,3 @@ function keyDown(event) {
 function keyUp(event) {
     keyboard[event.keyCode] = false;
 }
-
-window.addEventListener('keydown', keyDown);
-window.addEventListener('keyup', keyUp);
-
-window.onload = init;
